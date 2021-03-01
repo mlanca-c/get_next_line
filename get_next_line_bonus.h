02e41afd7dcb983eb4ad15e5212b883afd632829
mlanca-c <79856624+mlanca-c@users.noreply.github.com>
2021-03-01 15:05:36 +0000

    2nd try - Update GNL

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/27 14:30:23 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/03/01 11:18:09 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+int		has_return(char *save);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *s);
+
+#endif
