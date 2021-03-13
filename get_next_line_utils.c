e79efa9c414f9b371e0c2fdee7352357c9b0a721
mlanca-c <79856624+mlanca-c@users.noreply.github.com>
2021-03-13 19:39:20 +0000

    1st try - Update get_next_line

---
+++ get_next_line_utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/27 14:31:47 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/02/27 17:04:50 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		has_return(char *save)
+{
+	int		i;
+
+	if (!save)
+		return (0);
+	i = 0;
+	while (save[i])
+	{
+		if (save[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int		ft_strlen(char *s)
+{
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	char	*dst;
+
+	if (!(dst = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*tab;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	i = ft_strlen(s1) + ft_strlen(s2) + 1;
+	if (!(tab = (char *)malloc(sizeof(char) * i)))
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		tab[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+	{
+		tab[i] = s2[j];
+		i++;
+	}
+	tab[i] = '\0';
+	free(s1);
+	return (tab);
+}
